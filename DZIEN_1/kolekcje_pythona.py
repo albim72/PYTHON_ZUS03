33382c9c118c8bdd0bb9cfc70a36ef75831d97af
albim72 <marcin.albiniak72@gmail.com>
2025-03-24 10:22:45 +0100

    Create kolekcje_pythona.py

---
+++ DZIEN_1/kolekcje_pythona.py
@@ -0,0 +1,12 @@
+# --- Przykład: zarządanie biblioteką w Pythonie ----
+
+# Lista(list) -> lista książek w bibliotece
+# CTRL+D -> powielanie linii/bloku
+
+books = [
+    ("Wiedźmin","Andrzej Sapkowski",1990),
+    ("Pan Tadeusz","Adam Mickiewicz",1834),
+    ("Lalka","Bolesław Prus",1890),
+    ("Quo Vadis","Henryk Sienkiewicz",1896),
+    ("Faraon","Bolesław Prus",1897)
+]
