04ffe237309e2f238e7463d9f31eb7d9461d943e
albim72 <marcin.albiniak72@gmail.com>
2025-03-24 13:54:33 +0100

    Update analiza_dokumentu.py

--- DZIEN_1/analiza_dokumentu.py
+++ DZIEN_1/analiza_dokumentu.py
@@ -8,6 +8,7 @@ f(M) -> funkcja złożonej analizy
 
 from typing import List,Callable,Dict
 from collections import Counter
+from functools import partial
 import re
 
 def process_documents(docs:List[str],analisys_fn:Callable[[str],Dict[str,int]]) -> Dict[str,int]:
@@ -35,11 +36,28 @@ def word_frequency_analisys(text:str) -> Dict[str,int]:
     filtered_words = [w for w in words if len(w)>3 and w not in stopwords]
     return dict(Counter(filtered_words))
 
+def keyword_sentence_count(text:str,keyword:str="text")->Dict[str,int]:
+    """
+    funkcja liczy ile zdań w tekściezawiera wskazane słowo kluczowe
+    :param text:
+    :param keyword:
+    :return: słownik z liczbą zdań zawierających słowo kluczowe
+    """
+    sentences = re.split(f'[.!?]',text)
+    count = sum(1 for sentence in sentences if keyword.lower() in sentence.lower())
+    return {keyword:count}
+
 documents = [
-    "This is a sample document with some sample text and additional sample data.",
+    "This is a sample document with some sample text and additional sample data. Big text",
     "Another document, which contains different text and information from the first one.",
-    "Text mining and natural language processing are interesting fields."
+    "Text mining and natural language processing are interesting fields. My text"
 ]
 
 result = process_documents(documents,word_frequency_analisys)
 print(result)
+
+print("_"*60)
+
+analisys_with_keyword = partial(keyword_sentence_count,keyword="text")
+keyword_result = process_documents(documents,analisys_with_keyword)
+print(keyword_result)
