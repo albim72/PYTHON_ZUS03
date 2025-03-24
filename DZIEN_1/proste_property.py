d9930dc444da676bff92d3d168946d883febabd4
albim72 <marcin.albiniak72@gmail.com>
2025-03-24 15:31:53 +0100

    Update proste_property.py

--- DZIEN_1/proste_property.py
+++ DZIEN_1/proste_property.py
@@ -12,9 +12,23 @@ class BankAccount:
         if value<0:
             raise ValueError("Saldo nie może byc ujemne!")
         self._balance = value
-        
+
     #property tylko do odczytu
     @property
     def is_rich(self):
         return self._balance > 1_000_000
+
+
+konto = BankAccount("Jan Kowalski",50000)
+print(f"Aktualne saldo: {konto.balance} zł")
+konto.balance += 12300
+print(f"Nowe saldo: {konto.balance} zł")
+print(f"Czy właściciel ma status VIP? {konto.is_rich}")
+
+try:
+    konto.balance = -9000
+except ValueError as ve:
+    print(f"błąd: {ve}")
+
+
 
