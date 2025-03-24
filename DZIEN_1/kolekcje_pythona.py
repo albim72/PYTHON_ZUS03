7bc16c3bafc16cf992bb5a65ce12ded623364fd3
albim72 <marcin.albiniak72@gmail.com>
2025-03-24 11:04:05 +0100

    Update kolekcje_pythona.py

--- DZIEN_1/kolekcje_pythona.py
+++ DZIEN_1/kolekcje_pythona.py
@@ -22,3 +22,26 @@ authors = set()
 library_by_author = {}
 
 print(f"Godziny otwarcia biblioteki: {opening_hours[0]} - {opening_hours[1]}\n")
+
+#dodanie autorów do zbioru i organizacja książek w słowniku
+for title,author,year in books:
+    authors.add(author)
+    if author not in library_by_author:
+        library_by_author[author] = []
+    library_by_author[author].append((title,year))
+
+#lista książek w bibliotece
+for title,author,year in books:
+    print(f" - '{title}' ({year}), autor: {author}")
+
+# Unikalni autorzy w bibliotece
+print("\nUnikalni autorzy w bibliotece:")
+for author in authors:
+    print(f" - {author}")
+
+# Książki według autorów
+print("\nKsiążki według autorów:")
+for author,titles in library_by_author.items():
+    print(f"\nAutor: {author}")
+    for title,year in titles:
+        print(f" -> '{title}' ({year})")
