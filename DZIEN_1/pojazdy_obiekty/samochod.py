46f62b68b05d0a14298e07d9e2e343aa485099cc
albim72 <marcin.albiniak72@gmail.com>
2025-03-24 14:17:55 +0100

    Update samochod.py

--- DZIEN_1/pojazdy_obiekty/samochod.py
+++ DZIEN_1/pojazdy_obiekty/samochod.py
@@ -5,11 +5,15 @@ class Samochod:
         self.kolor = kolor
         #lewa strona - zmienna po kontekście, prawa strona - atrybut konstruktora init
 
+    # def __new__(cls, *args, **kwargs):
+    #     return object.__new__(Samochod)
+
+    def __repr__(self):
+        return f"Nowy obiekt klasy {self.__class__.__name__} -> {self.marka}"
+
     #zachowanie obiektu - metody
     def jedz(self):
         print(f"{self.marka} jedzie!")
 
     def info(self):
         print(f"Samochód marki {self.marka} jest koloru {self.kolor}.")
-
-        
