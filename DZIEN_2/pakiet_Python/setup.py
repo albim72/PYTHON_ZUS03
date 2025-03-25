6afd7398cb7d068dd5936d58e5cb5604dc7f3760
albim72 <marcin.albiniak72@gmail.com>
2025-03-25 11:00:54 +0100

    Create setup.py

---
+++ DZIEN_2/pakiet_Python/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='textutils',
+    version='0.1',
+    packages=find_packages(),
+    description='proste narzędzia procesowania tekstu',
+    author="Marcin",
+    install_requires = [],
+)
