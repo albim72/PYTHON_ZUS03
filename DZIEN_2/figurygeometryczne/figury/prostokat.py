a98cff6c34deaa1e65137120e7ce95ff5a9ff3c8
albim72 <marcin.albiniak72@gmail.com>
2025-03-25 09:11:07 +0100

    Create prostokat.py

---
+++ DZIEN_2/figurygeometryczne/figury/prostokat.py
@@ -0,0 +1,8 @@
+class Prostokat(Figura):
+    def __new__(cls,a,b):
+        if a==b:
+            return Kwadrat(bok=a)
+        return object.__new__(Prostokat)
+
+    def policz_pole(self):
+        return self.a*self.b
