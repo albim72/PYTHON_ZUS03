5a1511e14326f1c0c83b192ed1d8b971fc99d33e
albim72 <marcin.albiniak72@gmail.com>
2025-03-25 09:13:59 +0100

    Update main.py

--- DZIEN_2/figurygeometryczne/main.py
+++ DZIEN_2/figurygeometryczne/main.py
@@ -1,4 +1,15 @@
 from figury.trojkat import Trojkat
+from figury.prostokat import Prostokat
 
 tr = Trojkat(5.6,6.8)
 print(f'pole figury {tr.__class__.__name__} wynosi {tr.policz_pole():.2f} cm2')
+
+print("_"*50)
+pr1 = Prostokat(4.4,7.8)
+print(f'pole figury {pr1.__class__.__name__} wynosi {pr1.policz_pole():.2f} cm2')
+print(f'typ obiektu: {type(pr1)}')
+
+print("_"*50)
+pr2 = Prostokat(5.7,5.7)
+print(f'pole figury {pr2.__class__.__name__} wynosi {pr2.policz_pole():.2f} cm2')
+print(f'typ obiektu: {type(pr2)}')
