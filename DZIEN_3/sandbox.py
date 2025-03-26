4c0a37719c2e27c64dccfc6b15f3ed4c5820e418
albim72 <marcin.albiniak72@gmail.com>
2025-03-26 13:43:54 +0100

    Update sandbox.py

--- DZIEN_3/sandbox.py
+++ DZIEN_3/sandbox.py
@@ -10,6 +10,7 @@ def sandbox(user_code):
         'min':min,
         'max':max,
         'abs':abs,
+        'str':str
     }
 
     local_namespace = {}
@@ -31,7 +32,9 @@ def sandbox(user_code):
 
     return local_namespace,exec_time
 
+#przykład 1
 
+print("___ przykład 1 ___")
 user_code ="""
 result=[]
 for i in range(10):
@@ -40,4 +43,41 @@ for i in range(10):
 
 namespace, duration = sandbox(user_code)
 print(f"wynik: {namespace.get('result')}")
+print(f"czas wykonania: {duration:.5f} s")
+
+#przykład 2
+
+print("___ przykład 2 ___")
+user_code ="""
+result=[]
+for i in range(12):
+    result.append(abs(i*3))
+"""
+
+namespace, duration = sandbox(user_code)
+print(f"wynik: {namespace.get('result')}")
+print(f"czas wykonania: {duration:.5f} s")
+
+#przykład 3
+
+print("___ przykład 3 ___")
+user_code_unsafe ="""
+with open('secret.txt','r') as f:
+    data = f.read()
+"""
+
+namespace = sandbox(user_code_unsafe)
+print(f"wynik: {namespace}")
+# print(f"czas wykonania: {duration:.5f} s")
+
+#przykład 4
+
+print("___ przykład 4 ___")
+user_code_str ="""
+text = str(53432)
+print(text)
+"""
+
+namespace, duration = sandbox(user_code_str)
+print(f"wynik: {namespace}")
 print(f"czas wykonania: {duration:.5f} s")
