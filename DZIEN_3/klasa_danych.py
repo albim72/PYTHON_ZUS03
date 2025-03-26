330ce5f986cec83bea5429c2f23784bc27ad4d4d
albim72 <marcin.albiniak72@gmail.com>
2025-03-26 14:44:31 +0100

    Create klasa_danych.py

---
+++ DZIEN_3/klasa_danych.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from datetime import date
+
+@dataclass
+class Book:
+    title:str
+    author:str
+    published_year:int
+    pages:int
+    binding:str = "Paperback"
+
+    def age(self)->int:
+        current_year = date.today().year
+        return current_year - self.published_year
+
+#tworzenie obiektu klasy Book
+book = Book(title="1984",author="George Orwell",published_year=1949,pages=328)
+
+print(book)
+print(f"wiek książki: {book.age()} lat")
+
+book2 = Book(title="Wiedżmin",author="Andrzej Sapkowski",published_year=1986,pages=345,binding="Hardcover")
+print(book2)
+print(f"wiek książki: {book2.age()} lat")
