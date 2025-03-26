2be378d1658f3c95027072a822de8c10c3cc0c3d
albim72 <marcin.albiniak72@gmail.com>
2025-03-26 11:39:02 +0100

    Update main.py

--- DZIEN_3/internal_class/main.py
+++ DZIEN_3/internal_class/main.py
@@ -1,4 +1,26 @@
 from ccreate import create_class
+from valid import validator_factory
 
 obj = create_class("Alicja")
 print(obj.greet())
+
+print("_"*60)
+
+schema = {"name":str,"age":int}
+Validator = validator_factory(schema)
+
+data = {"name":"Janusz","age":"dwadzieścia"}
+validator = Validator(data)
+
+if not validator.validate():
+    print(f"Błędy walidacji: {validator.get_errors()}")
+else:
+    print("Dane poprawne!")
+
+data = {"name":"Anna","age":67}
+validator = Validator(data)
+
+if not validator.validate():
+    print(f"Błędy walidacji: {validator.get_errors()}")
+else:
+    print("Dane poprawne!")
