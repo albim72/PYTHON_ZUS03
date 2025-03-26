dc6f23ccf9dde59b4417d174c20d9a117c66d29e
albim72 <marcin.albiniak72@gmail.com>
2025-03-26 10:32:12 +0100

    Update runsession.py

--- DZIEN_3/zadanie_stat/runsession.py
+++ DZIEN_3/zadanie_stat/runsession.py
@@ -1,1 +1,14 @@
 
+class RunSession:
+    def __init__(self, distance_km:float, time_minutes:float, calories_burned:int):
+        self.distance_km = distance_km          # przebyty dystans w kilometrach
+        self.time_minutes = time_minutes        # czas biegu w minutach
+        self.calories_burned = calories_burned  # spalone kalorie
+
+    def average_pace(self):
+        """
+        Oblicza średnie tempo biegu w minutach na kilometr
+        """
+        if self.distance_km == 0:
+            return 0
+        return self.time_minutes / self.distance_km
