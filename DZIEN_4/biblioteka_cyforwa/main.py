c9597f36516fb58d5de19831384c40c95eddd5f1
albim72 <marcin.albiniak72@gmail.com>
2025-03-27 09:34:31 +0100

    Update main.py

--- DZIEN_4/biblioteka_cyforwa/main.py
+++ DZIEN_4/biblioteka_cyforwa/main.py
@@ -1,1 +1,18 @@
+if __name__ == "__main__":
+    author1 = Author("George", "Orwell", 1903)
+    author2 = Author("Harper", "Lee", 1926)
 
+    book1 = Book("1984", author1, 1949, "123-A")
+    book2 = Book("To Kill a Mockingbird", author2, 1960, "456-B")
+
+    user = User("Jan", "Kowalski")
+
+    user.borrow_book(book1)
+
+    print(book1.borrow_duration)  # np. 0
+    print(book1.status)  # borrowed
+
+    user.return_book(book1)
+    print(book1.status)  # available
+
+    serialize_to_json([author1, author2], [book1, book2], [user], "library_data.json")
