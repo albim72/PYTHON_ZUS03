8eb3c484efe35e5cea3e154dca242391e10759aa
albim72 <marcin.albiniak72@gmail.com>
2025-03-27 09:18:49 +0100

    Update main.py

--- DZIEN_4/klasy _danych/main.py
+++ DZIEN_4/klasy _danych/main.py
@@ -1,13 +1,25 @@
-from simpledt import Book
+from simpledt import Book,Author
 
 print("______ przykład 1 -> prosta dataclass Book ________")
-book1 = Book("1984","George Orwell",1949,"czerwona")
-book2 = Book("To Kill a Mockingbird","Harper Lee",1960,"niebieska")
-book3 = Book("Brave New World","Aldous Huxley",1932,"zielona")
-book4 = Book("Wiedźmin","Andrzej Sapkowski",1986,"czerwono-czarna")
+
+orwell = Author("George","Orwell")
+lee = Author("Harper","Lee")
+huxley = Author("Aldous","Huxley")
+sapkowski = Author("Andrzej","Sapkowski")
+
+books = [
+    Book("1984",1949,"czerwona",orwell),
+    Book("To Kill a Mockingbird",1960,"niebieska",lee),
+    Book("Brave New World",1932,"zielona",huxley),
+    Book("Wiedźmin",1986,"czerwono-czarna",sapkowski),
+    Book("Folwark zwierzęcy",1945,"żółta",orwell)
+]
 
 #opisy książek
-for book in (book1,book2,book3,book4):
+books_sorted = sorted(books,key=lambda b: b.author.last_name)
+
+print("Książki posortowane według nazwiska autora:\n")
+for book in books_sorted:
     print(book.description)
-    print(f"Wiek książki: {book.age()} lat\n")
+    print(f"wiek książki: {book.age()} lat")
     print("*"*60)
