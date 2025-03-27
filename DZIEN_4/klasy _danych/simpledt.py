263a17bdb47fbcea32d270cedbc93ef17272a86b
albim72 <marcin.albiniak72@gmail.com>
2025-03-27 09:09:22 +0100

    Update simpledt.py

--- DZIEN_4/klasy _danych/simpledt.py
+++ DZIEN_4/klasy _danych/simpledt.py
@@ -2,17 +2,24 @@ from dataclasses import dataclass
 from datetime import datetime
 
 @dataclass
+class Author:
+    first_name:str
+    last_name:str
+    
+    def full_name(self)->str:
+        return f"{self.first_name} {self.last_name}"
+
+@dataclass
 class Book:
     title:str
-    author:str
     year:int
     cover_color:str
+    author:Author
 
     @property
     def description(self)->str:
-        return f"'{self.title}' autorstwa {self.author} ({self.year}, okładka: {self.cover_color})"
-    
+        return f"'{self.title}' autorstwa {self.author.full_name()} ({self.year}, okładka: {self.cover_color})"
+
     def age(self)->str:
         current_year = datetime.now().year
         return current_year - self.year
-    
