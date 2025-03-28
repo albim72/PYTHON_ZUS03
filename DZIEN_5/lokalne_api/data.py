bbd8259871abdff3514828b71d001925f60c3e5f
albim72 <marcin.albiniak72@gmail.com>
2025-03-28 13:42:55 +0100

    Create data.py

---
+++ DZIEN_5/lokalne_api/data.py
@@ -0,0 +1,7 @@
+from .models import Book
+
+books = [
+    Book(id=1, title="1984", author="George Orwell", year=1949, genre="Dystopian"),
+    Book(id=2, title="Brave New World", author="Aldous Huxley", year=1932, genre="Science Fiction"),
+    Book(id=3, title="To Kill a Mockingbird", author="Harper Lee", year=1960)
+]
