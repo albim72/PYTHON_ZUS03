ac486b18b4c93e38e8d854a09eec65858db9c944
albim72 <marcin.albiniak72@gmail.com>
2025-03-28 10:35:49 +0100

    Update app.py

--- DZIEN_5/greet_flask_app/app.py
+++ DZIEN_5/greet_flask_app/app.py
@@ -7,7 +7,13 @@ def index():
     name = None
     if request.method == 'POST':
         name = request.form.get('name')
-    return render_template('index.html',name=name)
+    people = [
+        {"id":1,"name":"Anna","age":28},
+        {"id":2,"name":"Olaf","age":66},
+        {"id":3,"name":"Nadia","age":38},
+        {"id":4,"name":"Leon","age":23}
+    ]
+    return render_template('index.html',name=name,people=people)
 
 if __name__ == '__main__':
     app.run(debug=True)
