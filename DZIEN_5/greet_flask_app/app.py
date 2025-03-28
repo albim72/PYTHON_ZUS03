926d1c7ece629b70699ae874eea734749d3d8919
albim72 <marcin.albiniak72@gmail.com>
2025-03-28 10:42:31 +0100

    Update app.py

--- DZIEN_5/greet_flask_app/app.py
+++ DZIEN_5/greet_flask_app/app.py
@@ -1,18 +1,21 @@
 from flask import Flask,render_template,request
+from person import Person
 
 app = Flask(__name__)
+
+people_list = [
+    Person(1,"Anna",28),
+    Person(2,"Olaf",35),
+    Person(3,"Nadia",78),
+    Person(4,"Leon",57)
+]
 
 @app.route('/',methods=['GET','POST'])
 def index():
     name = None
     if request.method == 'POST':
         name = request.form.get('name')
-    people = [
-        {"id":1,"name":"Anna","age":28},
-        {"id":2,"name":"Olaf","age":66},
-        {"id":3,"name":"Nadia","age":38},
-        {"id":4,"name":"Leon","age":23}
-    ]
+    people = [p.to_dict() for p in people_list]
     return render_template('index.html',name=name,people=people)
 
 if __name__ == '__main__':
